1d31675fd629753cd63164ca122d84ff891a4e69
aldi nugroho <nugroho.aaldi@gmail.com>
2023-08-31 10:59:23 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+import * as React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import * as Pages from "./pages"
+
+class ChildRouting {
+    path = ''
+    element = <div></div>
+    constructor({
+        path = '',
+        element = <div></div>
+    }) {
+        this.path = path
+        this.element = element
+    }
+}
+
+class ParentRouting {
+    path = ''
+    element = <div></div>
+    childs = []
+    constructor({
+        path = '',
+        element = <div></div>,
+        childs = []
+    }) {
+        this.path = path
+        this.element = element
+        this.childs = childs.map((i) => new ChildRouting({path: `${path}${i?.path}`, element: i?.element}))
+    }
+}
+
+const  Routing = [
+    new ParentRouting({path: "/", element: <Pages.Main />})
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Routes>
+            {Routing.map((i,r) => (
+                <React.Fragment key={r}>
+                    <Route path={i.path} element={i.element} />
+                    {i?.childs.map((childs,ckey) => (
+                        <Route key={{ckey}} path={childs?.path} element={childs?.element} />
+                    ))}
+                </React.Fragment>
+            ))}
+        </Routes>
+      </Router>
   );
 }
 
