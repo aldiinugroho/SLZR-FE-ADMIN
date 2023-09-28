68a198ad524f4f05a1665f727fe2444a41b31b77
aldi nugroho <nugroho.aaldi@gmail.com>
2023-09-28 21:31:15 +0700

    update

--- src/pages/stok/childs/proses/index.js
+++ src/pages/stok/childs/proses/index.js
@@ -1,5 +1,7 @@
 import * as React from "react";
-import { Customalert, Custombody, Customheader, Customspinner, Sidebar } from "../../../../components";
+import { Customalert, Custombody, Custombutton, Customheader, Customnumtextfield, Customspinner, Customtextfield, Sidebar } from "../../../../components";
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
 function Index({
 }) {
@@ -26,7 +28,105 @@ function BodyComponent({
     }}>
       <h1>Proses Mobil</h1>
       <div className="spacingblack"></div>
+      <div style={{ padding: "10px" }}></div>
+      <FormSubmit />
     </div>
+  )
+}
+
+function FormSubmit({
+  type = "",
+  initialValues = {
+      name: "",
+      phone: "",
+      paymenttools: "",
+      ktp: "",
+      soldprice: "",
+      bookedfee: "",
+      leasing: ""
+  },
+  submit = () => {}
+}) {
+  return(
+    <Formik
+        initialValues={initialValues}
+        // validationSchema={submitformvalidation}
+        onSubmit={(values, { setSubmitting }) => {
+            submit(values)
+        }}
+    >
+        {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+            /* and other goodies */
+        }) => (
+            <React.Fragment>
+                <Customtextfield
+                    placeholder={"Nama"}
+                    value={values.nama}
+                    onChange={handleChange("nama")}
+                    onBlur={handleBlur("nama")}
+                    touched={touched.nama}
+                    errorMessage={errors.nama}
+                />
+                <div style={{ padding: 5 }}></div>
+                <Customtextfield
+                    placeholder={"Nomor Handphone"}
+                    value={values.nama}
+                    onChange={handleChange("nama")}
+                    onBlur={handleBlur("nama")}
+                    touched={touched.nama}
+                    errorMessage={errors.nama}
+                />
+                <div style={{ padding: 5 }}></div>
+                <Customtextfield
+                    placeholder={"Nomor KTP"}
+                    value={values.nama}
+                    onChange={handleChange("nama")}
+                    onBlur={handleBlur("nama")}
+                    touched={touched.nama}
+                    errorMessage={errors.nama}
+                />
+                <div style={{ padding: 5 }}></div>
+                <Customnumtextfield
+                    placeholder={"Harga Jual"}
+                    value={values.nama}
+                    onChange={handleChange("nama")}
+                    onBlur={handleBlur("nama")}
+                    touched={touched.nama}
+                    errorMessage={errors.nama}
+                />
+                <div style={{ padding: 5 }}></div>
+                <Customnumtextfield
+                    placeholder={"Booking Fee/DP"}
+                    value={values.nama}
+                    onChange={handleChange("nama")}
+                    onBlur={handleBlur("nama")}
+                    touched={touched.nama}
+                    errorMessage={errors.nama}
+                />
+                <div style={{ padding: 5 }}></div>
+                <Customtextfield
+                    placeholder={"Leasing"}
+                    value={values.nama}
+                    onChange={handleChange("nama")}
+                    onBlur={handleBlur("nama")}
+                    touched={touched.nama}
+                    errorMessage={errors.nama}
+                />
+                <div style={{ padding: 5 }}></div>
+                <Custombutton
+                    onClick={handleSubmit}
+                    title={"Proses"}
+                />
+            </React.Fragment>
+        )}
+    </Formik>
   )
 }
 
